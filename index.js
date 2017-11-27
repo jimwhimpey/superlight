ba251a9fc3b265d616799def3abee26bc13713f0
Jim Whimpey <jimwhimpey@me.com>
2017-11-26 20:57:11 -0800

    Sorting the photos properly

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ app.get('/photos/:page/:perpage', (req, res) => {
     if (err) console.log(err);
     if (!photos) console.log('Empty dir');
 
+    const sortedPhotos = photos.map(photo => ({
+        name: photo,
+        time: fs.statSync(`./assets/${photo}`).mtime.getTime()
+      }))
+      .sort(function(a, b) { return b.time - a.time; })
+      .map(function(photo) { return photo.name; });
+
     const firstfile = (req.params.page == 1) ? 0 : (req.params.page - 1) * req.params.perpage;
     const output = [];
 
     for (var i = firstfile; i < req.params.perpage * req.params.page; i++) {
-      if (!photos[i]) continue;
-      output.push(photos[i]);
+      if (!sortedPhotos[i]) continue;
+      output.push(sortedPhotos[i]);
     }
 
     res.json(output);
